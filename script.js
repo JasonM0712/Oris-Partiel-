9b4ba3e9c7d4c853b318160b8f2c1cf38e2b05fd
JasonM0712 <jasonmorel54@gmail.com>
2025-12-18 16:02:46 +0100

    Push 18/12/2025 Responsif

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
 'use strict';
+const burger = document.querySelector(".burger");
+const menuMobile = document.querySelector(".navButtonMobile");
 
-
-
+burger.addEventListener("click", () => {
+    menuMobile.classList.toggle("OpenBurger");
+    menuMobile.classList.toggle("closeBurger");
+});
